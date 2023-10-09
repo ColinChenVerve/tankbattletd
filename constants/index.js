2a09bec5c85e18ff2e4a4bac1e323e7c0b4f49c8
ColinChenVerve <133790502+ColinChenVerve@users.noreply.github.com>
2023-10-09 11:42:38 -0400

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,93 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Co-op Player in Regular Level Mode',
+  'PvP Battle to Destroy Opponents',
+  'Local Internet or Web Co-op',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/apple.svg',
+    title: 'Appstore',
+    subtitle: '',
+    link: 'https://apps.apple.com/ng/app/tank-battle-td/id1660289320', // Add the link property
+  },
+  {
+    imgUrl: '/playstore.svg',
+    title: 'Playstore',
+    subtitle: '',
+    link: 'https://play.google.com/store/apps/details?id=com.vervegroup.tankbattletd&hl=en&gl=US', // Add the link property
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/tankimg1.png',
+    title: 'Classic flavor enhanced with stunning 3D effects and models',
+    subtitle:
+        'A crazy fun Tower Defense game inspired by a retro game “Battle City” released in the 80s.',
+    webUrl: 'https://youtu.be/i-aqWVDnk_E?si=UzoqKtMWzpeaYpxB',
+  },
+  {
+    imgUrl: '/tankimg2.png',
+    title: 'Boss fights, air force, battleship, snow levels and many more',
+    subtitle:
+        'Experience two epic boss battles, receive airforce support, conquer challenging snow levels with varying ground conditions.',
+    webUrl: 'https://youtu.be/G-sXpibu1fs?si=aSUX_AKPgrFq98aS',
+  },
+  {
+    imgUrl: '/tankimg3.png',
+    title: 'Diverse Character Selection with Unique Vehicles',
+    subtitle:
+        'Choose from a variety of tanks, each equipped with unique weapon systems and stats, for an engaging combat experience.',
+    webUrl: 'https://youtu.be/rEhHE6T1gbs?si=GzA-LRjyzH4bwDmg',
+  },
+];
+
+export const socials = [
+  // {
+  //   name: 'twitter',
+  //   url: '/twitter.svg',
+  // },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    webUrl: 'https://www.linkedin.com/company/volckano',
+  },
+  // {
+  //   name: 'instagram',
+  //   url: '/instagram.svg',
+  // },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+    webUrl: 'https://www.facebook.com/volckanotech',
+  },
+];
